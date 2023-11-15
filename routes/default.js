ddf88537a493295618c5c2c2264d43d1db098676
root <root@DESKTOP-5N9IPDA>
2023-11-15 17:14:50 +0900

    test v2

--- routes/default.js
+++ routes/default.js
@@ -1,45 +1,61 @@
-const express  = require('express');
-const utils    = require('../utils');
+const express  = require("express");
 const router   = express.Router();
-const database = require('./db');
+const database = require("../db/db");
+const token    = require("../utils/token");
 
 /* Returns if user is logged in */
-router.get('/isLoggedIn', (request, response) => {
-    if(!request.session.loggedIn) {
-        response.json({
-            'status': 'failed'
-        })
-    } else {
-        response.json({
-            'status': 'ok'
-        })
-    }
-})
+router.get("/isLoggedIn", (request, response) => {
+	if(!request.session.loggedIn) {
+		response.json({
+			"status": "failed"
+		});
+	} else {
+		response.json({
+			"status": "ok"
+		});
+	}
+});
 
 /* Logs user out */
-router.get('/logout', (request, response) => {
-    request.session.loggedIn = false;
-    request.session.username = undefined;
+router.get("/logout", (request, response) => {
+	request.session.loggedIn = false;
+	request.session.username = undefined;
 
-    response.json({
-        'status': 'ok'
-    })
-})
+	response.json({
+		"status": "ok"
+	});
+});
 
 /* Returns personal info and THE SECRET INFORMATION */
-router.get('/personalInfo', (request, response) => {
-    if(!request.session.loggedIn) {
-        response.json({
-            'status': 'failed',
-            'message': 'Access denied'
-        })
-    } else {
-        response.json({
-            'status': 'ok',
-            'name': database[request.session.username].name,
-            'theSecret': '<img width="250px" src="img/theworstofthesecrets.jpg">'
-        })
-    }
-})
+router.get("/personalInfo", (request, response) => {
+	if(!request.session.loggedIn) {
+		response.json({
+			"status": "failed",
+			"message": "Access denied"
+		});
+	} else {
+		let tokenInfo = undefined,
+			//userInfo = database.users[request.session.username];
+			userInfo = database.getData("/users/"+ request.session.username);
+		if (userInfo.oneTimeToken) {            
+			if (userInfo.oneTimeToken.expires > new Date().getTime()) {
+				tokenInfo = { 
+					token: token.encode(userInfo.oneTimeToken.token),
+					expires: userInfo.oneTimeToken.expires 
+				};
+			} else {
+				tokenInfo = undefined;
+				userInfo.oneTimeToken = undefined;
+			}
+		}
+		response.json({
+			"status": "ok",
+			"authenticators": userInfo.authenticators,
+			"name": userInfo.name,
+			"oneTimeToken": tokenInfo,
+			"recoveryEmail": userInfo.recoveryEmail
+		});
+	}
+});
 
 module.exports = router;
